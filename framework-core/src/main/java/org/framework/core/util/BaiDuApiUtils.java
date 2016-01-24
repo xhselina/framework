ad0ffbd952c5269c98a21c4cd4781e45517d6d30
JeffreyJi <jeffreyji@yeah.net>
2016-01-24 22:26:10 +0800

    增加手机号归属地查询、unicodetoString 工具类

--- framework-core/src/main/java/org/framework/core/util/BaiDuApiUtils.java
+++ framework-core/src/main/java/org/framework/core/util/BaiDuApiUtils.java
@@ -1,30 +1,19 @@
 package org.framework.core.util;
 
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Executable;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
+ * 根据手机号，查询归属地  返回 json
  * baidu api utils
  * @version Ver 1.0
  * @Author <a href="mailto:jeffreyji@yeah.net">jeffrey</a>
@@ -32,21 +21,21 @@ import java.util.List;
  */
 public class BaiDuApiUtils {
 
-    public final static  String url = "http://apis.baidu.com/apistore/mobilenumber/mobilenumber?phone=18500237786";
+    public final static  String url = "http://apis.baidu.com/apistore/mobilenumber/mobilenumber?";
     public final static String apikey = "ec29d79adf7e0f620a5709988d52659b";
 
     public static String phoneBelongWhere(String phone){
         String result = "";
+        String urlParam = "phone=" + phone;
         // 创建默认的httpClient实例.
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(url);
+        HttpGet httpGet = new HttpGet(url + urlParam);
         // 创建httppost
 //        HttpPost httppost = new HttpPost(url);
 //        HttpGet httppost = new HttpGet(url);
         httpGet.setHeader("Content-Type", "application/json");
         httpGet.addHeader("charset","utf-8");
         httpGet.addHeader("apikey",apikey);
-
         //生成一个请求对象
         //生成一个http客户端对象
         HttpClient httpClient=new DefaultHttpClient();
@@ -57,7 +46,7 @@ public class BaiDuApiUtils {
             HttpEntity entity = response.getEntity();
             if (entity != null) {
                 // 读取请求内容
-                BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent(),"utf-8"));
+                BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent(),"UTF-8"));
                 String line = null;
                 StringBuilder sb = new StringBuilder();
                 while((line = br.readLine())!=null){
@@ -65,9 +54,9 @@ public class BaiDuApiUtils {
                 }
                 // 将资料解码
                 String reqBody = sb.toString();
-                System.out.println(reqBody);
-                result = new String(reqBody.getBytes("ISO-8859-1"));
-            }
+                result = EncodedUtils.unicode2String(reqBody);
+                System.out.println(result);
+        }
         }catch (Exception e) {
             e.printStackTrace();
         }  finally {
@@ -82,7 +71,7 @@ public class BaiDuApiUtils {
     }
 
     public static void main(String[] args) {
-        String result = phoneBelongWhere("18500237786");
+        String result = phoneBelongWhere("15940442369");
         System.out.println(result);
     }
 }
